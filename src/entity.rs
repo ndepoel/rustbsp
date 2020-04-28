a08c64dc6668b63d4bc42b7e5e1fbd4ffaec88c7
Nico de Poel <ndepoel@gmail.com>
2020-04-28 18:36:25 +0200

    Added some more unit tests, including handling of faulty input.

--- src/entity.rs
+++ src/entity.rs
@@ -144,8 +144,9 @@ mod tests
     use super::*;
 
     #[test]
-    pub fn test_tokenize()
+    fn test_tokenize()
     {
+        // Test a mix of quoted strings, spaces, tabs, newlines, longer whitespaces, interior nuls, and trailing whitespace
         let string = "This is \"a test\"   {  \tHello!   \0 \t\r\n   \"I am a longer quoted string\"   \"42 13.0 69\"     }\r\n\0".to_string();
         let mut chars = string.chars();
 
@@ -158,5 +159,68 @@ mod tests
         assert_eq!(Some("42 13.0 69".to_string()), next_token(&mut chars));
         assert_eq!(Some("}".to_string()), next_token(&mut chars));
         assert_eq!(None, next_token(&mut chars));
+    }
+
+    #[test]
+    fn test_vector()
+    {
+        // Test a mix of integer and floating point string values
+        let vec = parse_vector(&"1 2.0 3.14".to_string());
+        assert_eq!(1.0, vec.x);
+        assert_eq!(2.0, vec.y);
+        assert_eq!(3.14, vec.z);
+
+        // Missing tokens should result in a default component value
+        let vec = parse_vector(&"4 5".to_string());
+        assert_eq!(4.0, vec.x);
+        assert_eq!(5.0, vec.y);
+        assert_eq!(0.0, vec.z);
+
+        // Empty strings should be accepted, result in a default vector
+        let vec = parse_vector(&"".to_string());
+        assert_eq!(0.0, vec.x);
+        assert_eq!(0.0, vec.y);
+        assert_eq!(0.0, vec.z);
+
+        // Test parse errors, invalid numbers should result in default values
+        let vec = parse_vector(&"a b c".to_string());
+        assert_eq!(0.0, vec.x);
+        assert_eq!(0.0, vec.y);
+        assert_eq!(0.0, vec.z);
+    }
+
+    #[test]
+    fn test_entity()
+    {
+        let string = "{ 
+            \"classname\" \"foobar\"
+            \"origin\" \"1.1 2.2 3.3\"
+            \"bar\" \"baz\"
+        }".to_string();
+
+        let mut chars = string.chars();
+        let ent = parse_entity(&mut chars);
+        assert!(ent.is_some());
+
+        let entity = &ent.unwrap();
+        assert_eq!("foobar", &entity.class_name);
+        assert!(entity.origin.length() > 4.0);
+        assert!(entity.properties.contains_key("bar"));
+        assert_eq!("baz", entity.properties.get("bar").unwrap());
+    }
+
+    #[test]
+    fn test_broken()
+    {
+        assert!(parse_entity(&mut "".chars()).is_none());
+        assert!(parse_entity(&mut "}".chars()).is_none());
+        assert!(parse_entity(&mut "{".chars()).is_some());  // Just an opening bracket should result in an empty entity
+
+        // Broken off origin tag should still be accepted and result in a default origin vector
+        let ent = parse_entity(&mut "{ classname xyz origin".chars());
+        assert!(ent.is_some());
+        let entity = &ent.unwrap();
+        assert_eq!("xyz", &entity.class_name);
+        assert_eq!(0.0, entity.origin.length());
     }
 }
